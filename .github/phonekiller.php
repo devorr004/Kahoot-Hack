61380aa26fcee87a70b663df99d59cc4fd63bb5d
mohammadjavad1381 <51096320+mohammadjavad1381@users.noreply.github.com>
2022-10-04 15:20:42 +0330

    Add files via upload

---
+++ .github/phonekiller.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<body>
+<center>
+<img src="https://paulmaguire.me/wp-content/uploads/2018/11/SevenSevenThreeFour-720x281.png"width="300" height="150"class=”rotate90″> </center>
+</body>
+<head>
+<style>
+body   
+{   
+background-image:url("https://wallpaperaccess.com/download/hacker-iphone-1121342");
+background-repeat: no-repeat; 
+background-attachment: fixed; 
+background-size: 100% 100%; 
+color: #FFFFFF;
+}   
+</style>
+</head>
+<html>
+    <html lang= "en" >
+       <head>
+          <title>SMS Bomber</title>
+          <center><h1>ᴘʜᴏɴᴇ ᴋɪʟʟᴇʀ</h1></center>
+          <h3>⁪</h3>
+          <h4>⁪</h4>
+          <h5>⁪⁪</h5>
+          <h6>⁪</h6>
+          <h2>ᴀᴛᴛᴇɴᴛɪᴏɴ</h2>
+       </head>
+
+<body>
+    <p>𝙽𝚞𝚖𝚋𝚎𝚛 𝚜𝚝𝚊𝚛𝚝 𝚠𝚒𝚝𝚑𝚘𝚞𝚝 +98 or 0 </p>
+    <p>𝙼𝚊𝚡𝚒𝚖𝚞𝚖 𝚌𝚘𝚞𝚗𝚝 𝚒𝚜 20 </p> 
+</body>
+<body>
+<form action="sms.php" method="get">
+ᴘʜᴏɴᴇ: <input type="htmlspecialchars" name="phone"><br>
+ᴄᴏᴜɴᴛ: <input type="htmlspecialchars" name="count"><br>
+<input type="submit">
+</form>
+</body>
+<body>
+<p>⁪</p>
+</body>
+<body>
+<a href="https://t.me/mj_fekri">
+   <img 
+src="https://1000logos.net/wp-content/uploads/2021/04/Telegram-logo.png" alt="the Telegram logo"width="60" height="37"/>
+</a>
+</body>
+<body>
+<a href="https://www.instagram.com/m0h4m4d_1053r/">
+   <img
+src="https://1000logos.net/wp-content/uploads/2017/02/Instagram-Logo-768x432.png" alt="the Instagram logo"width="70" height="36"/>
+</a>
+</a>
+</body>
+<body>
+    <style>
+        h1 {color: floralwhite}
+        h2 {color: crimson}
+        p {color: orange}  
+        form {color: yellowgreen}
+</body>       
+</html>
+>
+body>       
+</html>
+>
+reen}
+</body>       
+</html>
+>
+body>       
+</html>
+>
+lor: floralwhite}
+        h2 {color: crimson}
+        p {color: orange}  
+        form {color: yellowgreen}
+</body>       
+</html>
+>
+body>       
+</html>
+>
